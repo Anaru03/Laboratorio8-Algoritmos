fdd59019f2d5047a1f33c93773d679070547de24
Anaru03 <dele22428@uvg.edu.gt>
2025-10-05 19:00:52 -0600

    Main

--- Ejercicio_1/Ejercicio_1.c
+++ Ejercicio_1/Ejercicio_1.c
@@ -11,3 +11,17 @@ void function(int n) {
         }
     }
 }
+
+int main() {
+    int n_values[] = {1, 10, 100, 1000, 10000};
+    int num_values = sizeof(n_values) / sizeof(n_values[0]);
+    clock_t start, end;
+    double cpu_time_used;
+
+    printf("===========================================\n");
+    printf("  Resultados del Ejercicio 1\n");
+    printf("  Complejidad Teorica: O(n^2 log n)\n");
+    printf("===========================================\n\n");
+
+    printf("%-15s %-25s\n", "Size (n)", "Tiempo (segundos)");
+    printf("-------------------------------------------\n");
